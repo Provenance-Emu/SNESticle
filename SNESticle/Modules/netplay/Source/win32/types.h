29829f9b633150ebc5acd1cf32894b4c8508064a
Joseph Mattello <mail@joemattiello.com>
2022-07-30 05:00:37 -0400

    prov: fix uint64 def
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- SNESticle/Modules/netplay/Source/win32/types.h
+++ SNESticle/Modules/netplay/Source/win32/types.h
@@ -38,14 +38,14 @@ typedef unsigned short		Uint16;
 typedef signed short		Int16;
 typedef unsigned int		Uint32;
 typedef signed int			Int32;
-typedef unsigned __int64	Uint64;
-typedef signed __int64		Int64;
+typedef unsigned long long	Uint64;
+typedef signed long long		Int64;
 typedef char				Char;
 typedef unsigned char		Bool;
 typedef float				Float32;
 typedef double				Float64;
 
 #define _ALIGN(__Align) 
-#define _INLINE _inline
+#define _INLINE inline
 
 #endif
